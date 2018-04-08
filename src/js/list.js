b3b9de308814a7bd2fb2e315b5edffe99c173f3b
cc <1169413007@qq.com>
2018-04-08 18:48:51 +0800

    列表页商品排版

--- src/js/list.js
+++ src/js/list.js
@@ -13,6 +13,7 @@ jQuery(function($){
 
         $('.header').css('box-shadow','0 3px 5px #ccc');  })
 
+        // 轮播图
         $('.mainLeft .banner').carousel_c({
         "width":210,
         "height":400,
@@ -23,4 +24,19 @@ jQuery(function($){
         "className":['page2','active2'],
         "imgs":['../imgs/wb_lis_ban1.jpg','../imgs/wb_lis_ban2.jpg','../imgs/wb_lis_ban3.jpg','../imgs/wb_lis_ban4.jpg']
     });
+
+        // 关键字搜索模块
+        let kwBrands = $('.kwBrands');
+        let kwkwType = $('.kwType');
+        let brandsKW = '浪琴 爱宝时 天梭 迪沃斯 欧米茄 劳力士 赫柏林 CK 百达翡丽 菲拉格慕 艾美达 诺美纳 帝舵 积家 阿玛尼 万国 宝玑 宝珀 美度 库尔沃 莫勒 柏高 荣汉斯 卡西欧'.split(' ');
+
+        let typeKW = '机械 自动机械 手动机械 石英 光能 智能 电波 人动电能 陀飞轮 电子 其它'.split(' ');    
+        createKeyWord(kwBrands,brandsKW);
+        createKeyWord(kwkwType,typeKW);
+        function createKeyWord(container,kwArr){
+            container.append($.map(kwArr,function(item){
+                return `<li><a href="#">${item}</a></li>`;
+                     
+            }));
+        }
 })
